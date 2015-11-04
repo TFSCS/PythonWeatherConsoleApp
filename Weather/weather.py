df4cafbb4a230687b3b28fa8e61550a2571a7444
collin <collin.mickels@tallulahfalls.org>
2015-11-04 17:19:05 +0000

    First Commit

---
+++ Weather/weather.py
@@ -0,0 +1,30 @@
+import json
+import urllib.request
+
+print ("===================================================")
+print ("TFS COMPUTER SCIENCE CLUB PYTHON WEATHER APP v0.1.0")
+print ("===================================================")
+print ("\n\n\n\n")
+  
+
+zip = (input("Enter Your Zip Code: "))
+apiURL = 'http://api.wunderground.com/api/API_KEY/geolookup/conditions/q/' + zip + '.json'
+# JSON Parser
+with urllib.request.urlopen(apiURL) as url:
+    response = url.read()
+charset = url.info(). get_content_charset('utf-8')  # UTF-8 is the JSON defaultjson_string = f.json
+json_string = json.loads(response.decode(charset))
+parsed_json = json_string
+
+
+# Parsed Variables (Temp, Humidity, Conditions)
+location = parsed_json['location']['city']
+currentTempF = parsed_json['current_observation']['temp_f']
+cuttentFeelsLikeTempF = parsed_json['current_observation']['feelslike_string']
+currentRelativeHumidity = parsed_json['current_observation']['relative_humidity']
+
+
+print ('Current temperature in ' + str(location) + ' is: ' + str(currentTempF) + ' deg. F')
+
+
+
