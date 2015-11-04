921bbb11cbcaed7cd0146a1c8b9acaf8a1dfb48b
collin <collin.mickels@tallulahfalls.org>
2015-11-04 22:16:58 +0000

    Cleaned up code, added Functionality

--- Weather/weather.py
+++ Weather/weather.py
@@ -7,12 +7,9 @@ print ("===================================================")
 print ("\n\n\n\n")
   
 
-zip = (input("Enter Your Zip Code: "))
-
-API_KEY = open('API_KEY.txt', encoding="utf-8").read()
-
-apiURL = 'http://api.wunderground.com/api/' + API_KEY + '/geolookup/conditions/q/' + zip + '.json'
-
+zip = (input("Enter Your Zip Code: ")) # Get Zipcode
+API_KEY = open('API_KEY.txt', encoding="utf-8").read() # Past API KEY in text file within this directory.
+apiURL = 'http://api.wunderground.com/api/' + API_KEY + '/geolookup/conditions/q/' + zip + '.json' # apiURL Builder
 
 # JSON Parser
 with urllib.request.urlopen(apiURL) as url:
@@ -23,13 +20,20 @@ parsed_json = json_string
 
 
 # Parsed Variables (Temp, Humidity, Conditions)
-location = parsed_json['location']['city']
-currentTempF = parsed_json['current_observation']['temp_f']
-cuttentFeelsLikeTempF = parsed_json['current_observation']['feelslike_string']
-currentRelativeHumidity = parsed_json['current_observation']['relative_humidity']
+location = str(parsed_json['location']['city'])
+weather = str(parsed_json['current_observation']['weather'])
+currentTempF = str(parsed_json['current_observation']['temp_f'])
+cuttentFeelsLikeTempF = str(parsed_json['current_observation']['feelslike_string'])
+currentRelativeHumidity = str(parsed_json['current_observation']['relative_humidity'])
 
+print ('===================================================')
+print ('Weather For: ' + location )
+print ('===================================================')
+print ('Weather: ' + weather)
+print ('Current Temperature: ' + currentTempF)
+print ('Feels Like: ' + cuttentFeelsLikeTempF)
+print ('Humidity: ' + currentRelativeHumidity)
 
-print ('Current temperature in ' + str(location) + ' is: ' + str(currentTempF) + ' deg. F')
 
 
 
