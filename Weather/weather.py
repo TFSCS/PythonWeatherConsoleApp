f95f2b01d44cbd2de4bc30caf78b0e6b15fdb92a
collin <collin.mickels@tallulahfalls.org>
2015-11-06 13:58:33 +0000

    Added API error check

--- Weather/weather.py
+++ Weather/weather.py
@@ -4,6 +4,11 @@ import json
 import urllib.request
 import os
 import sys
+# Create and format API URL
+API_KEY = open('API_KEY.txt', encoding="utf-8").read() # Paste API KEY in text file within this directory.
+if '<insert your api key here>' in API_KEY:
+    print ('ERROR: Place your api key in the "API_KEY.txt" file')
+    sys.exit()
 
 # Clears the console and prints header
 def printHeader():
@@ -17,11 +22,8 @@ printHeader()# execute
 # Get the user's zip code.
 zip = (input("|Enter Your Zip Code: "))
 
-# Create and format API URL
-API_KEY = open('API_KEY.txt', encoding="utf-8").read() # Paste API KEY in text file within this directory.
+# JSON Fetch and Parser
 apiURL = 'http://api.wunderground.com/api/' + API_KEY + '/geolookup/conditions/q/' + zip + '.json' # apiURL Builder
-
-# JSON parser
 with urllib.request.urlopen(apiURL) as url:
     response = url.read()
 charset = url.info(). get_content_charset('utf-8')  # UTF-8 Encode
