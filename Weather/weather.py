7bf2062d55cf2467d8f8a871c5b16f9d57684e7a
collin <collin.mickels@tallulahfalls.org>
2015-11-12 14:33:19 +0000

    Added support for inputting the names of a city and state or country for the location. Added parameters for visibility and wind.

--- Weather/weather.py
+++ Weather/weather.py
@@ -4,6 +4,7 @@ import json
 import urllib.request
 import os
 import sys
+
 # Create and format API URL
 API_KEY = open('API_KEY.txt', encoding="utf-8").read() # Paste API KEY in text file within this directory.
 if '<insert your api key here>' in API_KEY:
@@ -13,17 +14,35 @@ if '<insert your api key here>' in API_KEY:
 # Clears the console and prints header
 def printHeader():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("\n                _\n              (`  ).                   _\n             (     ).              .:(`  )`.\n)           _(       '`.          :(   .    )\n        .=(`(      .   )     .--  `.  (    ) )\n       ((    (..__.:'-'   .+(   )   ` _`  ) )\n`.     `(       ) )       (   .  )     (   )  ._\n  )      ` __.:'   )     (   (   ))     `-'.-(`  )\n)  )  ( )       --'       `- __.'         :(      ))\n.-'  (_.'          .')                    `(    )  ))\n                  (_  )                     ` __.:'\n\n--..,___.--,--'`,---..-.--+--.,,-,,..._.--..-._.------.")
     print ("|====================================================================")
     print ("|TFS COMPUTER SCIENCE PYTHON WEATHER APP v0.1.0")
     print ("|====================================================================")
 printHeader()# execute
 
-# Get the user's zip code.
-zip = (input("|Enter Your Zip Code: "))
+# Get selection of inputting either a zip code or a city.
+print ("Please select whether to use a zip code or city")
+print ("1) Zip \n2)City\n")
+selection = (input(""))
 
-# JSON Fetch and Parser
-apiURL = 'http://api.wunderground.com/api/' + API_KEY + '/geolookup/conditions/q/' + zip + '.json' # apiURL Builder
+#Clear console
+printHeader()
+
+# Set global API URL
+global apiURL
+    
+# Creating API URL based on the selection
+if selection == "1":
+    zip = (input("Enter Your Zip Code: "))
+    printHeader()
+    apiURL = 'http://api.wunderground.com/api/' + API_KEY + '/geolookup/conditions/q/' + zip + '.json' # apiURL Builder
+elif selection == "2":
+    city = (input("|Enter Your City: "))
+    printHeader()
+    state = (input("|Enter Your State or Country: "))
+    printHeader()
+    apiURL = 'http://api.wunderground.com/api/' + API_KEY + '/geolookup/conditions/q/' + state + '/' + city + '.json' 
+
+# Fetch and parse JSON
 with urllib.request.urlopen(apiURL) as url:
     response = url.read()
 charset = url.info(). get_content_charset('utf-8')  # UTF-8 Encode
@@ -32,28 +51,27 @@ parsed_json = json_string
 
 # Check for an invalid zip code input
 if 'No cities match your search query' in str(response):
-    print ('ERROR: INVALID ZIP')
+    print ('ERROR: INVALID LOCATION')
     sys.exit()
 
 # Parsed Variables (Temp, Humidity, Conditions)
-locationCity = str(parsed_json['location']['city'])
-locationState = str(parsed_json['location']['state'])
+locationFull = str(parsed_json['current_observation']['display_location']['full'])
 weather = str(parsed_json['current_observation']['weather'])
-wTemp = str(parsed_json['current_observation']['temperature_string'])
-wFeelsLikeTemp = str(parsed_json['current_observation']['feelslike_string'])
-wRelativeHumidity = str(parsed_json['current_observation']['relative_humidity'])
-wPressure = str(parsed_json['current_observation']['pressure_in'])
+temp = str(parsed_json['current_observation']['temperature_string'])
+feelsLikeTemp = str(parsed_json['current_observation']['feelslike_string'])
+relativeHumidity = str(parsed_json['current_observation']['relative_humidity'])
+pressure = str(parsed_json['current_observation']['pressure_in'])
+visibilityM = str(parsed_json['current_observation']['visibility_mi'])
+windM = str(parsed_json['current_observation']['wind_mph'])
 
 # Output
 printHeader()
-print ('|Weather For: ' + locationCity + ' ' + locationState )
+print ('|Weather For: ' + locationFull )
 print ("|====================================================================")
 print ('|Weather: ' + weather)
-print ('|Temperature: ' + wTemp)
-print ('|Feels Like: ' + wFeelsLikeTemp)
-print ('|Humidity: ' + wRelativeHumidity)
-print ('|Pressure: ' + wPressure)
-
-
-
-
+print ('|Temperature: ' + temp)
+print ('|Feels Like: ' + feelsLikeTemp)
+print ('|Humidity: ' + relativeHumidity)
+print ('|Pressure: ' + pressure)
+print ('|Visibility: ' + visibilityM)
+print ('|Wind Speed: ' + windM)
